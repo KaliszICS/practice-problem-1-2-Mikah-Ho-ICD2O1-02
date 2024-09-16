a0a3eba12b9e4b772a62a88cffca2baaf3cff14a
Mikah-Ho-ICD2O1-02 <mikah.ho@student.tdsb.on.ca>
2024-09-16 17:50:49 +0000

    questions completed

--- PP1_2.py
+++ PP1_2.py
@@ -1,28 +1,48 @@
-
-
-
+'''
+    Lesson: Variables and Data Types
+    Author: Mikah Ho
+    Date Created: Sept 16, 2024
+    Date Last Modified: Sept 16, 2024
+'''
 
 def q1():
   #Write Assignment code here
+  name = "Alex"
 
 def q2():
   #Write Assignment code here
+  three = 3
+  five = 5
+  two = 2
+  print(three + five + two)
 
 def q3():
   #Write Assignment code here
+  twentyOne = 21
+  four = 4
+print(21 // 4)
 
 def q4():
   #Write Assignment code here
+  sixPointThree = 6.3
+  threePointZero = 3.0
+  print(6.3 / 3.0)
 
 def q5():
   #Write Assignment code here
+  string = "Hello World"
+  print(string)
+  string = "Goodbye World"
 
 def q6():
   #Write Assignment code here
+  yes = True
 
 def q7():
   #Write Assignment code here
-
+seven = 7
+two = 2
+print(seven % two)
   
 #DO NOT EDIT ANYTHING AFTER THIS COMMENT
 q1()
